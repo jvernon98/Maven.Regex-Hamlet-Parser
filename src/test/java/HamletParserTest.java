dbb095868481e37aa087e9d8051f5057169903f2
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-04-11 10:25:23 -0400

    had to fix the test, now they all pass

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -30,7 +30,7 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHoratioToTariq() {
-        hamletParser.changeHamletToTariq();
+        hamletParser.changeHoratioToTariq();
 
         Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(hamletParser.getHamletData());
